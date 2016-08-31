cc780c9bbc2a6b168e0e653bf78b9bb4805f4084
OriolG <download.spam.net@gmail.com>
2016-08-31 15:00:44 +0200

    bat class finished

--- The Quest/Bat.cs
+++ The Quest/Bat.cs
@@ -9,11 +9,18 @@ namespace The_Quest
 
         public override void Move(Random random)
         {
-            if(random.Next(2) == 1)
+            if(random.Next(1, 2) == 1)
             {
-                
+                location = Move(FindPlayerDirection(game.PlayerLocation), game.Boundaries);                
             }
-                
+            else
+            {
+                location = Move((Direction)random.Next(1, 4), game.Boundaries);
+            }
+            if (NearPlayer())
+            {
+                game.HitPlayer(2, random);
+            }
         }
     }
 }
